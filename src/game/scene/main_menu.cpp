37fa3fb5b990c89852001fc55ba43fdb513d3bda
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-18 23:04:44 +0100

    draw basic button

--- src/game/scene/main_menu.cpp
+++ src/game/scene/main_menu.cpp
@@ -27,5 +27,24 @@ void MainMenuScene::render(const Game& game) const {
 	const int text_width = Raylib_MeasureTextEx(font, text, font_size, font_spacing).x;
 	const float pos_x = (game.window.width() - text_width) / 2;
 	const float pos_y = (game.window.height() - font_size) / 2;
-	Raylib_DrawTextEx(font, text, Vector2 { pos_x, pos_y }, font_size, font_spacing, WHITE);
+	const Vector2 padding = { 4, 4 };
+	const Vector2 font_adjust = { -1, -2 };
+	Raylib_DrawRectangle(
+		pos_x - padding.x + font_adjust.x,
+		pos_y - padding.y + font_adjust.y,
+		text_width + 2 * padding.x,
+		font_size + 2 * padding.y,
+		Color { 196, 196, 196, 255 }
+	);
+	Raylib_DrawRectangleLinesEx(
+		{
+			pos_x - padding.x + font_adjust.x,
+			pos_y - padding.y + font_adjust.y,
+			text_width + 2 * padding.x,
+			font_size + 2 * padding.y,
+		},
+		2,
+		Color { 130, 130, 130, 255 }
+	);
+	Raylib_DrawTextEx(font, text, Vector2 { pos_x, pos_y }, font_size, font_spacing, BLACK);
 }
