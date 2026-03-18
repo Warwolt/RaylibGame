742776385409d1e5862275252474024a9d906520
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-18 22:52:41 +0100

    draw with font

--- src/game/scene/main_menu.cpp
+++ src/game/scene/main_menu.cpp
@@ -20,11 +20,12 @@ void MainMenuScene::update(Game* game) {
 }
 
 void MainMenuScene::render(const Game& game) const {
+	const Font& font = game.resources.default_font();
 	const int font_size = 32;
-	const char* text = "Main Menu";
-	const int text_width = Raylib_MeasureText(text, font_size);
-	const int pos_x = (game.window.width() - text_width) / 2;
-	const int pos_y = (game.window.height() - font_size) / 2;
-	Raylib_DrawText(text, pos_x, pos_y, font_size, WHITE);
-	Raylib_ClearBackground(Color { 0, 127, 127, 255 });
+	const float font_spacing = 0.0f;
+	const char* text = "New Game";
+	const int text_width = Raylib_MeasureTextEx(font, text, font_size, font_spacing).x;
+	const float pos_x = (game.window.width() - text_width) / 2;
+	const float pos_y = (game.window.height() - font_size) / 2;
+	Raylib_DrawTextEx(font, text, Vector2 { pos_x, pos_y }, font_size, font_spacing, WHITE);
 }
