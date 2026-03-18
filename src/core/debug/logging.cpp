aa76a117bbf3ab954865895c9a07ffc4c9ff1d38
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-18 10:42:17 +0100

    dummy test

--- src/core/debug/logging.cpp
+++ src/core/debug/logging.cpp
@@ -29,7 +29,7 @@
 #define BACKGROUND_CYAN "\033[46m"
 #define BACKGROUND_WHITE "\033[47m"
 
-static bool g_unit_test_mode = false;
+static bool g_should_add_colors = true;
 
 static const char* log_level_to_str(LogLevel level) {
 	switch (level) {
@@ -88,35 +88,31 @@ void initialize_logging() {
 	SetConsoleMode(hOut, dwMode);
 }
 
-void initialize_unit_test_logging() {
-	g_unit_test_mode = true;
-	initialize_logging();
+void disable_log_colors() {
+	g_should_add_colors = false;
 }
 
 void debug_log(LogLevel log_level, const char* filename, int line, const char* fmt, ...) {
 	const bool debugger_is_present = IsDebuggerPresent();
-	const bool should_add_formatting = !g_unit_test_mode;
 	char buffer[1024] = { 0 };
 	int offset = 0;
 
-	if (should_add_formatting) {
-		/* Add color */
-		if (!debugger_is_present) {
-			offset += snprintf(buffer + offset, 1024 - offset, log_level_color(log_level));
-		}
-
-		/* Add timestamp */
-		time_t t = time(0); // get time now
-		struct tm* now = localtime(&t);
-		offset += snprintf(buffer + offset, 1024 - offset, "%02d-%02d-%02d ", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday); // year
-		offset += snprintf(buffer + offset, 1024 - offset, "%02d:%02d ", now->tm_hour, now->tm_min); // hour
-
-		/* File and line */
-		offset += snprintf(buffer + offset, 1024 - offset, "%s:%d ", filename_from_path(filename), line);
-
-		/* Log level */
-		offset += snprintf(buffer + offset, 1024 - offset, "%s ", log_level_to_str(log_level));
+	/* Add color */
+	if (g_should_add_colors && !debugger_is_present) {
+		offset += snprintf(buffer + offset, 1024 - offset, log_level_color(log_level));
 	}
+
+	/* Add timestamp */
+	time_t t = time(0); // get time now
+	struct tm* now = localtime(&t);
+	offset += snprintf(buffer + offset, 1024 - offset, "%02d-%02d-%02d ", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday); // year
+	offset += snprintf(buffer + offset, 1024 - offset, "%02d:%02d ", now->tm_hour, now->tm_min); // hour
+
+	/* File and line */
+	offset += snprintf(buffer + offset, 1024 - offset, "%s:%d ", filename_from_path(filename), line);
+
+	/* Log level */
+	offset += snprintf(buffer + offset, 1024 - offset, "%s ", log_level_to_str(log_level));
 
 	/* Add message */
 	va_list args;
@@ -134,7 +130,7 @@ void debug_log(LogLevel log_level, const char* filename, int line, const char* f
 		bool is_error = log_level == LogLevel_Error || log_level == LogLevel_Fatal;
 		FILE* stream = is_error ? stderr : stdout;
 		fprintf(stream, buffer);
-		if (should_add_formatting) {
+		if (g_should_add_colors) {
 			fprintf(stream, COLOR_RESET);
 		}
 		fflush(stream);
--- test/main.cpp
+++ test/main.cpp
@@ -3,8 +3,8 @@
 #include "core/debug/logging.h"
 
 int main(int argc, char** argv) {
-	// initialize_unit_test_logging();
-	// enable_unit_test_debug_exit();
+	initialize_logging();
+	disable_log_colors();
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
