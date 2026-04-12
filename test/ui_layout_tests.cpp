affbc7056fcd66e2a88e4ecddecf81c3faa3c700
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-04-12 18:35:01 +0200

    BoxLayout_100_100_Gives_50_50

--- test/ui_layout_tests.cpp
+++ test/ui_layout_tests.cpp
@@ -1,3 +1,5 @@
+#include "game/ui.h"
+
 #include "core/debug/logging.h"
 #include "test/snapshots.h"
 
@@ -7,8 +9,7 @@
 #include <filesystem>
 #include <functional>
 
-constexpr int SCREEN_WIDTH = 768;
-constexpr int SCREEN_HEIGHT = 432;
+constexpr Vector2 SCREEN_SIZE = { 768, 432 };
 
 class UILayoutTests : public ::testing::Test {
 public:
@@ -23,8 +24,8 @@ public:
 	}
 };
 
-Image render_image(int width, int height, std::function<void()> render) {
-	RenderTexture2D texture = Raylib_LoadRenderTexture(width, height);
+Image render_image(std::function<void()> render) {
+	RenderTexture2D texture = Raylib_LoadRenderTexture(SCREEN_SIZE.x, SCREEN_SIZE.y);
 	Raylib_BeginTextureMode(texture);
 	render();
 	Raylib_EndTextureMode();
@@ -33,10 +34,30 @@ Image render_image(int width, int height, std::function<void()> render) {
 	return image;
 }
 
-TEST_F(UILayoutTests, HelloWorld) {
-	Image image = render_image(SCREEN_WIDTH, SCREEN_HEIGHT, []() {
-		Raylib_ClearBackground(RED);
-		Raylib_DrawText("Test2", 0, 0, 16, WHITE);
-	});
+TEST_F(UILayoutTests, BoxLayout_100_100_Gives_50_50) {
+	ResourceManager resources;
+	ui::Element root = {
+		.content = ui::Box {
+			.direction = ui::Direction::Horizontal,
+			.children = {
+				ui::Element {
+					.style = {
+						.width = ui::RelativeSize(100),
+						.background_color = RED,
+					},
+				},
+				ui::Element {
+					.style = {
+						.width = ui::RelativeSize(100),
+						.background_color = GREEN,
+					},
+				}
+			}
+		},
+	};
+
+	ui::layout_element(resources, SCREEN_SIZE, &root);
+	Image image = render_image([&]() { ui::draw_element(resources, root); });
+
 	EXPECT_SNAPSHOT_EQ(image);
 }
--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
 # TODO
-- Snapshot testing for UI
 - QoL: Render something while hot reload build is on going. Give visual feedback if hot reload went ok.
 - MainMenu scene with mouse+keyboard buttons
 - Gameplay scene with basic camera + character movement
@@ -8,7 +7,11 @@
 - Integrate Tracy profiler so we can measure performance of UI system
 - Bug: Fix clang format not formatting nested designated initializers
 
+# Doing
+- Test the layout UI code
+
 # Done
+- Snapshot testing for UI
 - Prototype basic UI rendering
 - Basic Scene system
 - Add Window and fullscreen toggling
