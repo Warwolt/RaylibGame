f65d012e30bf8845c5b760cf460990c26d2bdbe2
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-04-12 19:26:18 +0200

    text

--- test/ui_layout_tests.cpp
+++ test/ui_layout_tests.cpp
@@ -102,3 +102,68 @@ TEST_F(UILayoutTests, BoxLayout_100_25_100_Gives_37_25_37) {
 	EXPECT_EQ(std::get<ui::Box>(root.content).children[2].layout.margin_box.width, SCREEN_WIDTH * 0.375);
 	EXPECT_SNAPSHOT_EQ(image);
 }
+
+TEST_F(UILayoutTests, BoxLayout_100_WithMultipleParagraphs) {
+	ResourceManager resources;
+	resources.load_default_font("resource/font/ModernDOS8x16.ttf");
+	ui::Element root = {
+		.style = {
+			.width = ui::RelativeSize(100),
+			.padding = ui::Spacing::uniform(20),
+		},
+		.content = ui::Box {
+			.direction = ui::Direction::Vertical,
+			.children = {
+				ui::Element {
+					.style = {
+						.margin = {
+							.bottom = 16,
+						},
+						.font_size = 32,
+					},
+					.content = ui::Text {
+						.text = "Super Metroid",
+					}
+				},
+				ui::Element {
+					.style = {
+						.margin = {
+							.bottom = 16,
+						},
+					},
+					.content = ui::Text {
+						.text = "Samus Aran brings the last Metroid to the Ceres space colony for scientific study. Investigation of the specimen, a larva, reveals that its energy-producing abilities could be harnessed for the good of civilization.",
+					}
+				},
+				ui::Element {
+					.style = {
+						.margin = {
+							.bottom = 16,
+						},
+					},
+					.content = ui::Text {
+						.text = "Shortly after leaving, Samus receives a distress call alerting her to return to the colony immediately. She finds the scientists dead, and the Metroid larva stolen by Ridley, leader of the Space Pirates. Samus escapes during a self-destruct sequence and follows Ridley to the planet Zebes. She searches the planet for the Metroid and finds that the Pirates have rebuilt their base there.",
+					}
+				},
+				ui::Element {
+					.style = {
+						.margin = {
+							.bottom = 16,
+						},
+					},
+					.content = ui::Text {
+						.text = "After defeating three bosses in various regions of Zebes, Samus confronts Ridley in his lair and defeats him, only to discover that the capsule containing the Metroid larva has been shattered and the larva is missing. She then heads for Tourian, the heart of the Space Pirates' base, and fights several Metroids that have reproduced. Samus confronts the Metroid larva, which has grown to enormous size. It attacks and nearly kills Samus, but relents at the last moment. As Samus was present at its hatching on SR388, the Metroid has imprinted on Samus, and recognizes her as its \"mother\".",
+					}
+				},
+			}
+		},
+	};
+
+	ui::layout_element(resources, SCREEN_SIZE, &root);
+	Image image = render_image([&]() {
+		Raylib_ClearBackground(BLACK);
+		ui::draw_element(resources, root);
+	});
+
+	EXPECT_SNAPSHOT_EQ(image);
+}
