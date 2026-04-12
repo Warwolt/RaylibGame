b447af2c33fa82ba7c6a3a3ad1c248159b90a18c
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-04-12 18:50:03 +0200

    box layout tests

--- test/ui_layout_tests.cpp
+++ test/ui_layout_tests.cpp
@@ -9,7 +9,9 @@
 #include <filesystem>
 #include <functional>
 
-constexpr Vector2 SCREEN_SIZE = { 768, 432 };
+constexpr int SCREEN_WIDTH = 768;
+constexpr int SCREEN_HEIGHT = 432;
+constexpr Vector2 SCREEN_SIZE = { SCREEN_WIDTH, SCREEN_HEIGHT };
 
 class UILayoutTests : public ::testing::Test {
 public:
@@ -25,7 +27,7 @@ public:
 };
 
 Image render_image(std::function<void()> render) {
-	RenderTexture2D texture = Raylib_LoadRenderTexture(SCREEN_SIZE.x, SCREEN_SIZE.y);
+	RenderTexture2D texture = Raylib_LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
 	Raylib_BeginTextureMode(texture);
 	render();
 	Raylib_EndTextureMode();
@@ -59,5 +61,44 @@ TEST_F(UILayoutTests, BoxLayout_100_100_Gives_50_50) {
 	ui::layout_element(resources, SCREEN_SIZE, &root);
 	Image image = render_image([&]() { ui::draw_element(resources, root); });
 
+	EXPECT_EQ(std::get<ui::Box>(root.content).children[0].layout.margin_box.width, SCREEN_WIDTH / 2);
+	EXPECT_EQ(std::get<ui::Box>(root.content).children[1].layout.margin_box.width, SCREEN_WIDTH / 2);
+	EXPECT_SNAPSHOT_EQ(image);
+}
+
+TEST_F(UILayoutTests, BoxLayout_100_25_100_Gives_37_25_37) {
+	ResourceManager resources;
+	ui::Element root = {
+		.content = ui::Box {
+			.direction = ui::Direction::Horizontal,
+			.children = {
+				ui::Element {
+					.style = {
+						.width = ui::RelativeSize(100),
+						.background_color = RED,
+					},
+				},
+				ui::Element {
+					.style = {
+						.width = ui::RelativeSize(25),
+						.background_color = GREEN,
+					},
+				},
+				ui::Element {
+					.style = {
+						.width = ui::RelativeSize(100),
+						.background_color = BLUE,
+					},
+				}
+			}
+		},
+	};
+
+	ui::layout_element(resources, SCREEN_SIZE, &root);
+	Image image = render_image([&]() { ui::draw_element(resources, root); });
+
+	EXPECT_EQ(std::get<ui::Box>(root.content).children[0].layout.margin_box.width, SCREEN_WIDTH * 0.375);
+	EXPECT_EQ(std::get<ui::Box>(root.content).children[1].layout.margin_box.width, SCREEN_WIDTH * 0.25);
+	EXPECT_EQ(std::get<ui::Box>(root.content).children[2].layout.margin_box.width, SCREEN_WIDTH * 0.375);
 	EXPECT_SNAPSHOT_EQ(image);
 }
