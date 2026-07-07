ef1efd6eed4bf511cf23379e16b93bec878ed118
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-07-07 22:20:02 +0200

    Add assert

--- test/snapshot_tests/snapshots_main.cpp
+++ test/snapshot_tests/snapshots_main.cpp
@@ -2,11 +2,13 @@
 
 #include "test/snapshot_tests/snapshots.h"
 
+#include "core/debug/assert.h"
 #include "core/debug/logging.h"
 
 int main(int argc, char** argv) {
 	initialize_logging();
 	disable_log_colors();
+	disable_assert_message_box();
 	testing::InitGoogleTest(&argc, argv);
 	snapshots::initialize_snapshot_tests(argc, argv);
 
--- test/unit_tests/unit_test_main.cpp
+++ test/unit_tests/unit_test_main.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 
+#include "core/debug/assert.h"
 #include "core/debug/logging.h"
 
 int main(int argc, char** argv) {
 	initialize_logging();
 	disable_log_colors();
+	disable_assert_message_box();
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
