e4a2e34334b63f26ad314b05c137a175c5a28a3d
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-07-14 15:54:00 +0200

    clean up main.cpp with HotReloading struct

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ struct GameLibrary {
 	decltype(&Game_render) render;
 };
 
-static GameLibrary load_library(const std::string& library_path) {
+static GameLibrary load_game_library(const std::string& library_path) {
 	HMODULE handle = LoadLibraryA(library_path.c_str());
 	if (!handle) {
 		DWORD error_code = GetLastError();
@@ -40,6 +40,7 @@ static GameLibrary load_library(const std::string& library_path) {
 
 static Game* g_game_state = nullptr;
 
+// FIXME: move into `HotReloading::update`
 static void on_build_command_done(int exit_code) {
 	if (exit_code == 0) {
 		g_game_state->debug.reload_state = HotReloadState::ReadyToReload;
@@ -49,51 +50,40 @@ static void on_build_command_done(int exit_code) {
 	}
 }
 
-int main(int argc, char** argv) {
-	Win32_enable_crash_handler();
-	Win32_set_process_dpi_aware();
-	initialize_logging();
+struct HotReloading {
+	std::string executable_directory;
+	std::string library_name;
+	std::string library_copy_name;
 
-	/* Initialize Tracy */
-	PROFILING_STARTUP_PROFILER();
-
-	/* Get executable directory */
-	std::string executable_directory = Win32_get_executable_directory();
-
-	/* Get library path */
-	std::string library_name = "GameLib.dll";
-	std::string library_path = executable_directory + "\\" + library_name;
-
-	/* Check library exists */
-	if (!Win32_file_exists(library_path)) {
-		ABORT("Can't find %s", library_name.c_str());
+	std::string library_path() const {
+		return this->executable_directory + "\\" + this->library_name;
 	}
 
-	/* Copy library */
-	std::string library_copy_name = "GameLib-hot-reload.dll";
-	std::string library_copy_path = executable_directory + "\\" + library_copy_name;
-	Win32_copy_file(library_path, library_copy_path);
+	std::string library_copy_path() const {
+		return this->executable_directory + "\\" + this->library_copy_name;
+	}
 
-	/* Load copy */
-	GameLibrary game_library = load_library(library_copy_name);
+	GameLibrary load_library() const {
+		/* Check library exists */
+		if (!Win32_file_exists(library_path())) {
+			ABORT("Can't find %s", this->library_name.c_str());
+		}
 
-	/* State */
-	g_game_state = game_library.initialize(argc, argv);
+		/* Copy library */
+		Win32_copy_file(library_path(), library_copy_path());
 
-	/* Run program */
-	while (!g_game_state->should_quit) {
-		/* Run game */
-		game_library.update(g_game_state);
-		game_library.render(*g_game_state);
+		/* Load copy */
+		return load_game_library(this->library_copy_name);
+	}
 
-		/* Update hot reloading */
-		switch (g_game_state->debug.reload_state) {
+	void update(Game* game_state, GameLibrary* game_library) {
+		switch (game_state->debug.reload_state) {
 			case HotReloadState::Idle:
 			case HotReloadState::Failed: {
 				/* Trigger rebuild */
 				if (Raylib_IsKeyPressed(KEY_F5)) {
 					LOG_INFO("Rebuilding game library");
-					g_game_state->debug.reload_state = HotReloadState::Rebuilding;
+					game_state->debug.reload_state = HotReloadState::Rebuilding;
 					Win32_run_command("cmake --build build --target Library", on_build_command_done);
 				}
 
@@ -104,20 +94,48 @@ int main(int argc, char** argv) {
 				break;
 
 			case HotReloadState::ReadyToReload: {
-				g_game_state->debug.reload_state = HotReloadState::Idle;
+				game_state->debug.reload_state = HotReloadState::Idle;
 
 				/* Unload library */
-				FreeLibrary(game_library.handle);
+				FreeLibrary(game_library->handle);
 
 				/* Copy rebuilt library */
-				Win32_copy_file(library_path, library_copy_path);
+				Win32_copy_file(library_path(), library_copy_path());
 
 				/* Reload copied library */
-				game_library = load_library(library_copy_name);
+				*game_library = load_game_library(this->library_copy_name);
 				LOG_INFO("Game library reloaded");
 				PROFILING_LOG("Game library reloaded");
 			} break;
 		}
+	}
+};
+
+int main(int argc, char** argv) {
+	Win32_enable_crash_handler();
+	Win32_set_process_dpi_aware();
+	initialize_logging();
+
+	/* Initialize Tracy */
+	PROFILING_STARTUP_PROFILER();
+
+	HotReloading hot_reloading = {
+		.executable_directory = Win32_get_executable_directory(),
+		.library_name = "GameLib.dll",
+		.library_copy_name = "GameLib-hot-reload.dll",
+	};
+
+	/* Load copy */
+	GameLibrary game_library = hot_reloading.load_library();
+
+	/* State */
+	g_game_state = game_library.initialize(argc, argv);
+
+	/* Run program */
+	while (!g_game_state->should_quit) {
+		game_library.update(g_game_state);
+		game_library.render(*g_game_state);
+		hot_reloading.update(g_game_state, &game_library);
 	}
 
 	/* Shutdown */
