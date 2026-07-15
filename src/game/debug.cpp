855590eb90fec276a6454e8d41a388d66d1687e5
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-07-15 14:47:03 +0200

    move animation def

--- src/game/debug.cpp
+++ src/game/debug.cpp
@@ -8,15 +8,14 @@
 using namespace std::chrono_literals;
 
 void render_debug_overlay(const Game& game) {
-	const std::vector<AnimationFrame<std::string>> frames = {
-		{ "Rebuilding.", 500ms },
-		{ "Rebuilding..", 500ms },
-		{ "Rebuilding...", 500ms },
-	};
-	const std::string text = current_animation_frame(frames, game.debug.reload_state.last_changed(), Time::now());
-
 	switch (game.debug.reload_state.value()) {
 		case HotReloadState::Rebuilding: {
+			const std::vector<AnimationFrame<std::string>> frames = {
+				{ "Rebuilding.", 500ms },
+				{ "Rebuilding..", 500ms },
+				{ "Rebuilding...", 500ms },
+			};
+			const std::string text = current_animation_frame(frames, game.debug.reload_state.last_changed(), Time::now());
 			Raylib_DrawRectangle(0, 0, game.window.width(), game.window.height(), Color { 0, 0, 0, 127 });
 			Raylib_DrawTextEx(game.resources.get_font(FontID(0)), text.c_str(), { 4, 0 }, 32, 0, YELLOW);
 		} break;
