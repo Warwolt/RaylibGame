e12b45b828a8fbcc322151e84f828de4c8617520
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-07-18 21:29:40 +0200

    unit tests for ElementTree

--- test/unit_tests/ui_element_tree_tests.cpp
+++ test/unit_tests/ui_element_tree_tests.cpp
@@ -4,7 +4,7 @@
 
 #include "game/resource.h"
 
-TEST(UIElementTreeTests, ForEach_PreOrder) {
+ui::ElementTree enumerated_tree() {
 	ui::ElementTree tree;
 
 	tree.push_element(ui::Element { .debug_name = "First" });
@@ -15,8 +15,27 @@ TEST(UIElementTreeTests, ForEach_PreOrder) {
 	tree.close_element();
 	tree.push_element(ui::Element { .debug_name = "Fourth" });
 
+	return tree;
+}
+
+TEST(UIElementTreeTests, RangeBasedLoop_IteratesInPreOrder) {
+	ui::ElementTree tree = enumerated_tree();
+
 	std::vector<std::string> debug_names;
 	for (const ui::Element& element : tree) {
+		debug_names.push_back(element.debug_name);
+	}
+
+	const std::vector<std::string> expected = { "root", "First", "Second", "Third", "Fourth" };
+	EXPECT_EQ(debug_names, expected);
+}
+
+TEST(UIElementTreeTests, ForLoop_IteratesInPreOrder) {
+	ui::ElementTree tree = enumerated_tree();
+
+	std::vector<std::string> debug_names;
+	for (auto it = tree.begin(); it != tree.end(); ++it) {
+		const ui::Element& element = *it;
 		debug_names.push_back(element.debug_name);
 	}
 
