2de91a8a086bf141e825dd5e5c073c2c67aa2014
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-07-26 23:52:33 +0200

    Add note to Game struct

--- src/game/game.h
+++ src/game/game.h
@@ -6,6 +6,11 @@
 #include "platform/input.h"
 #include "platform/window.h"
 
+// The state storage of the whole game.
+//
+// NOTE: To support hot reloading, you may NEVER store a function pointer here!
+// If you do, the program will likely crash on reload since the function
+// pointer will point into the library that was just unloaded.
 struct Game {
 	bool should_quit = false;
 	Debug debug;
